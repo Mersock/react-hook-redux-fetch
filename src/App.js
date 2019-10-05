6705c9cfaf2b35e2f991de71adebee8168d14496
Phumthawan <knzphumthawan@gmail.com>
2019-10-05 13:05:22 +0700

    initial redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import Posts from './components/Posts'
 import Postform from './components/Postform';
+import { Provider } from 'react-redux'
+import store from './store';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Postform/>
-      <hr/>
-      <Posts/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
