deebb9cc8b5928e8c89604a41634e575f1535bfb
Phumthawan <knzphumthawan@gmail.com>
2019-10-05 11:19:31 +0700

    add post components

---
+++ src/components/Posts.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+
+function Posts() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch(`http://jsonplaceholder.typicode.com/posts`)
+            .then(res => res.json())
+            .then(data => setPosts(data))
+        return () => {
+
+        };
+    }, []);
+
+    const postItem = posts.map(post => (
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+        </div>
+    ))
+    return (
+        <div>
+            {postItem}
+        </div>
+    )
+}
+
+export default Posts
