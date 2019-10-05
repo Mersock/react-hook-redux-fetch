4f0f7dcccb92e20b87b09c0cb367d9466055e2f5
Phumthawan <knzphumthawan@gmail.com>
2019-10-05 13:33:50 +0700

    initail store action reducer

---
+++ src/actions/postActions.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react'
+import { FETCH_POSTS, NEW_POSTS } from './types'
+
+export const fetchPosts = () => dispatch => {
+    useEffect(() => {
+        fetch(`http://jsonplaceholder.typicode.com/posts`)
+            .then(res => res.json())
+            .then(data => dispatch({
+                type: FETCH_POSTS,
+                payload: data
+            }))
+    }, []);
+
+}
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,24 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { connect } from 'react-redux'
 
 function Posts() {
-    const [posts, setPosts] = useState([])
 
-    useEffect(() => {
-        fetch(`http://jsonplaceholder.typicode.com/posts`)
-            .then(res => res.json())
-            .then(data => setPosts(data))
-        return () => {
-
-        };
-    }, []);
-
-    const postItem = posts.map(post => (
-        <div key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-        </div>
-    ))
-    
     return (
         <div>
             {postItem}
