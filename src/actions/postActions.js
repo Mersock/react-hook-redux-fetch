f3ebf9763578f6febb688d7fdef0bc3d883e7296
Phumthawan <knzphumthawan@gmail.com>
2019-10-06 16:41:33 +0700

    fetch post with hook

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,14 +1,10 @@
-import { useEffect } from 'react'
 import { FETCH_POSTS, NEW_POSTS } from './types'
 
 export const fetchPosts = () => dispatch => {
-    useEffect(() => {
-        fetch(`http://jsonplaceholder.typicode.com/posts`)
-            .then(res => res.json())
-            .then(data => dispatch({
-                type: FETCH_POSTS,
-                payload: data
-            }))
-    }, []);
-
+    fetch(`http://jsonplaceholder.typicode.com/posts`)
+        .then(res => res.json())
+        .then(data => dispatch({
+            type: FETCH_POSTS,
+            payload: data
+        }))
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,13 +1,25 @@
-import React, { useEffect, useState } from 'react'
-import { connect } from 'react-redux'
+import React, { useEffect } from 'react'
+import PropTypes from "prop-types";
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchPosts } from '../actions/postActions'
 
 function Posts() {
+    const dispatch = useDispatch()
+    const posts = useSelector(state => state.posts.items)
 
+    useEffect(() => {
+        dispatch(fetchPosts())
+    }, [dispatch])
     return (
         <div>
-            {postItem}
+            
         </div>
     )
 }
+
+Posts.propTypes = {
+    fetchPosts: PropTypes.func.isRequired,
+    posts:PropTypes.array.isRequired
+};
 
 export default Posts
